3cb4b50eecdd270e6e08c8f55b5feb7343531cdd
Ryan Haveson <rhaveson@qti.qualcomm.com>
2017-03-31 17:29:19 -0700

    up to module 3

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var os = require("os");
 var express = require('express');
 var mongoose = require('mongoose');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
@@ -9,12 +12,30 @@ var port = process.env.PORT || 3000;
 
 
 var bookRouter = express.Router();
-bookRouter .route('/Books')
+bookRouter.route('/Books')
     .get((req, res) => {
-        var responseJson = {hello: "This is my api"};
-        res.json(responseJson);
+        var query = {};
+        if(req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, (err, books) => {
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(books); 
+        });
     });
-    
+
+bookRouter.route('/Books/:bookId')
+    .get((req, res) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(book); 
+        });
+    });
+
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
@@ -22,6 +43,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
+    
     console.log("running my server on Cloud9 -> https://" + process.env.C9_HOSTNAME + ":" + port);
 });
 
